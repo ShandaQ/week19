98d9c3e69395b776cac40b2d525a604c2ec53d89
ShandaQ <shandaqkDevelopment@gmail.com>
2021-12-31 10:52:46 -0600

    optimize iamges and lazy loading

---
+++ assets/js/events.js
@@ -0,0 +1,60 @@
+/*
+checked if the current page was either 
+events.html, tickets.html, or schedule.html 
+so we could apply the correct DOM manipulation methods 
+specific to that page.
+*/
+// if (window.location.href.indexOf("tickets") > -1) {...}
+
+/*
+DOM manipulation will now be linked to each HTML page 
+directly through bundles,
+remove the checker and add DOM .ready method
+*/
+require("bootstrap");
+const createEl = require("./domMethods");
+const createLoremIpsum = require("./helpers");
+
+$(document).ready(function () {
+    const currentEvent = JSON.parse(localStorage.getItem("currentEvent")) || {
+        title: "Title Placeholder",
+        subtitle: "",
+        description: ""
+    };
+
+    const pageEl = document.querySelector("#page");
+
+    const containerEl = createEl("div", {
+            class: "container"
+        },
+        createEl("div", {
+                class: "card mb-3"
+            },
+            createEl("img", {
+                class: "card-img-top",
+                style: "width: 5px",
+                src: currentEvent.image || "https://via.placeholder.com/350x150"
+            }),
+            createEl("div", {
+                    class: "card-body"
+                },
+                createEl("h1", {
+                    class: "card-title"
+                }, currentEvent.title || ""),
+                createEl("h2", {
+                    class: "text-muted"
+                }, currentEvent.subtitle || ""),
+                createEl("p", {
+                    class: "card-text mt-3"
+                }, currentEvent.description || createLoremIpsum(100)),
+                createEl("a", {
+                    class: "btn btn-primary",
+                    href: "tickets.html"
+                }, "Buy Tickets")
+            )
+        ),
+
+    )
+    pageEl.appendChild(containerEl)
+
+})
---
+++ assets/js/tickets.js
@@ -0,0 +1,44 @@
+/*
+checked if the current page was either 
+events.html, tickets.html, or schedule.html 
+so we could apply the correct DOM manipulation methods 
+specific to that page.
+*/
+// if (window.location.href.indexOf("tickets") > -1) {...}
+
+/*
+DOM manipulation will now be linked to each HTML page 
+directly through bundles,
+remove the checker and add DOM .ready method
+*/
+require("bootstrap");
+const createEl = require("./domMethods");
+
+$(document).ready(function () {
+
+    const purchaseBtn = document.getElementById("purchaseBtn");
+    const purchaseEmail = document.getElementById("purchaseEmail");
+    const modalEl = document.querySelector(".modal-content");
+    const modalBodyEl = document.querySelector(".modal-body");
+    const modalFooterEl = document.querySelector(".modal-footer");
+
+
+    function purchaseTicket() {
+
+        modalEl.removeChild(modalBodyEl)
+        modalEl.removeChild(modalFooterEl)
+
+        modalEl.append(createEl("div", {
+                class: "modal-body"
+            },
+            createEl("h5", {
+                    class: "modal-title"
+                },
+                `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`
+            ),
+        ))
+
+    }
+    purchaseBtn.addEventListener("click", purchaseTicket);
+
+})
